ff363f19ad569398b718b149e8d7e05f6220e841
xlei <727090531@qq.com>
2020-04-15 09:17:38 +0800

    inital project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Button,Form,FormItem,Input,Message} from "element-ui";
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Message)
 
 new Vue({
   router,
