e23bc6a2e86ca7e080ae3d884ea41528ebcdcd17
727090531 <55726793+727090531@users.noreply.github.com>
2020-05-24 15:43:41 +0800

    Update StaAll.vue
    complish

--- src/views/sta/StaAll.vue
+++ src/views/sta/StaAll.vue
@@ -254,7 +254,254 @@
 
 <script>
     export default {
-        name: "StaAll"
+        name: "StaAll"，
+data() {
+            return {
+                searchValue: {
+                    departmentId: null,
+                },
+                title: '',
+                showAdvanceSearchView: false,
+                allDeps: [],
+                emps: [],
+                loading: false,
+                popVisible: false,
+                popVisible2: false,
+                dialogVisible: false,
+                total: 0,
+                page: 1,
+                keyword: '',
+                size: 10,
+                tiptopDegrees: ['本科', '大专', '硕士', '博士', '高中', '初中', '小学', '其他'],
+                options: [{
+                    value: '选项1',
+                    label: '黄金糕'
+                }, {
+                    value: '选项2',
+                    label: '双皮奶'
+                }, {
+                    value: '选项3',
+                    label: '蚵仔煎'
+                }, {
+                    value: '选项4',
+                    label: '龙须面'
+                }, {
+                    value: '选项5',
+                    label: '北京烤鸭'
+                }],
+                inputDepName: '所属部门',
+                emp: {
+                    name: "javaboy",
+                    gender: "男",
+
+                    email: "laowang@qq.com",
+                    phone: "18565558897",
+                    address: "深圳市南山区",
+                    departmentId: null,
+                    tiptopDegree: "本科",
+                    specialty: "信息管理与信息系统",
+
+                },
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                },
+                rules: {
+                    name: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    gender: [{required: true, message: '请输入性别', trigger: 'blur'}],
+                    email: [{required: true, message: '请输入邮箱地址', trigger: 'blur'}, {
+                        type: 'email',
+                        message: '邮箱格式不正确',
+                        trigger: 'blur'
+                    }],
+                    phone: [{required: true, message: '请输入电话号码', trigger: 'blur'}],
+                    address: [{required: true, message: '请输入员工地址', trigger: 'blur'}],
+                    departmentId: [{required: true, message: '请输入部门名称', trigger: 'blur'}],
+                    tiptopDegree: [{required: true, message: '请输入学历', trigger: 'blur'}],
+                    specialty: [{required: true, message: '请输入专业', trigger: 'blur'}],
+
+                }
+            }
+        },
+        mounted() {
+            this.initEmps();
+            this.initData();
+            this.initPositions();
+        },
+        methods: {
+            searvhViewHandleNodeClick(data) {
+                this.inputDepName = data.name;
+                this.searchValue.departmentId = data.id;
+                this.popVisible2 = !this.popVisible2
+            },
+            exportData() {
+                window.open('/employee/basic/export', '_parent');
+            },
+            emptyEmp() {
+                this.emp = {
+                    name: "",
+                    gender: "",
+                    email: "",
+                    phone: "",
+                    departmentId: null,
+                    salary:null,
+                    tiptopDegree: "",
+                    specialty: "",
+                }
+                this.inputDepName = '';
+            },
+            showEditEmpView(data) {
+                this.initPositions();
+                this.title = '编辑员工信息';
+                this.emp = data;
+                this.inputDepName = data.department.name;
+                this.dialogVisible = true;
+            },
+            deleteEmp(data) {
+                this.$confirm('此操作将永久删除【' + data.name + '】, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequest("/employee/basic/" + data.id).then(resp => {
+                        if (resp) {
+                            this.initEmps();
+                        }
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            doAddEmp() {
+                if (this.emp.id) {
+                    this.$refs['empForm'].validate(valid => {
+                        if (valid) {
+                            this.putRequest("/employee/basic/", this.emp).then(resp => {
+                                if (resp) {
+                                    this.dialogVisible = false;
+                                    this.initEmps();
+                                }
+                            })
+                        }
+                    });
+                } else {
+                    this.$refs['empForm'].validate(valid => {
+                        if (valid) {
+                            this.postRequest("/employee/basic/", this.emp).then(resp => {
+                                if (resp) {
+                                    this.dialogVisible = false;
+                                    this.initEmps();
+                                }
+                            })
+                        }
+                    });
+                }
+            },
+            handleNodeClick(data) {
+                this.inputDepName = data.name;
+                this.emp.departmentId = data.id;
+                this.popVisible = !this.popVisible
+            },
+            showDepView() {
+                this.popVisible = !this.popVisible
+            },
+            showDepView2() {
+                this.popVisible2 = !this.popVisible2
+            },
+            initPositions() {
+                this.getRequest('/employee/basic/positions').then(resp => {
+                    if (resp) {
+                        this.positions = resp;
+                    }
+                })
+            },
+            getMaxWordID() {
+                this.getRequest("/employee/basic/maxWorkID").then(resp => {
+                    if (resp) {
+                        this.emp.workID = resp.obj;
+                    }
+                })
+            },
+            initData() {
+                if (!window.sessionStorage.getItem("nations")) {
+                    this.getRequest('/employee/basic/nations').then(resp => {
+                        if (resp) {
+                            this.nations = resp;
+                            window.sessionStorage.setItem("nations", JSON.stringify(resp));
+                        }
+                    })
+                } else {
+                    this.nations = JSON.parse(window.sessionStorage.getItem("nations"));
+                }
+                if (!window.sessionStorage.getItem("joblevels")) {
+                    this.getRequest('/employee/basic/joblevels').then(resp => {
+                        if (resp) {
+                            this.joblevels = resp;
+                            window.sessionStorage.setItem("joblevels", JSON.stringify(resp));
+                        }
+                    })
+                } else {
+                    this.joblevels = JSON.parse(window.sessionStorage.getItem("joblevels"));
+                }
+                if (!window.sessionStorage.getItem("politicsstatus")) {
+                    this.getRequest('/employee/basic/politicsstatus').then(resp => {
+                        if (resp) {
+                            this.politicsstatus = resp;
+                            window.sessionStorage.setItem("politicsstatus", JSON.stringify(resp));
+                        }
+                    })
+                } else {
+                    this.politicsstatus = JSON.parse(window.sessionStorage.getItem("politicsstatus"));
+                }
+                if (!window.sessionStorage.getItem("deps")) {
+                    this.getRequest('/employee/basic/deps').then(resp => {
+                        if (resp) {
+                            this.allDeps = resp;
+                            window.sessionStorage.setItem("deps", JSON.stringify(resp));
+                        }
+                    })
+                } else {
+                    this.allDeps = JSON.parse(window.sessionStorage.getItem("deps"));
+                }
+            },
+            sizeChange(currentSize) {
+                this.size = currentSize;
+                this.initEmps();
+            },
+            currentChange(currentPage) {
+                this.page = currentPage;
+                this.initEmps('advanced');
+            },
+            showAddEmpView() {
+                this.emptyEmp();
+                this.title = '添加员工';
+                this.getMaxWordID();
+                this.dialogVisible = true;
+            },
+            initEmps(type) {
+                this.loading = true;
+                let url = '/employee/basic/?page=' + this.page + '&size=' + this.size;
+                if (type && type == 'advanced') {
+
+
+                    if (this.searchValue.departmentId) {
+                        url += '&departmentId=' + this.searchValue.departmentId;
+                    }
+                } else {
+                    url += "&name=" + this.keyword;
+                }
+                this.getRequest(url).then(resp => {
+                    this.loading = false;
+                    if (resp) {
+                        this.emps = resp.data;
+                        this.total = resp.total;
+                    }
+                });
+            }
+        }
     }
 </script>
 
